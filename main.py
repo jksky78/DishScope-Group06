af2b320b2649c7f8d525fed9551d641dac05cfef
Jun Kit <jksky7813@gmail.com>
2026-09-04 01:33:18 +0800

    Created error messages and added email field to user database

--- main.py
+++ main.py
@@ -12,22 +12,40 @@ def home ():
 @app.route("/register", methods=["GET", "POST"])
 def create_table():
     name = ""
+    errors = []
     if request.method == "POST":
         connection = sqlite3.connect('test.db')
         cursor = connection.cursor()
         table2 = 'drop table users'
         table = '''create table if not exists users(
                 ID integer primary key autoincrement,
-                name text,
-                password text
+                name text not null,
+                password text not null,
+                email text not null
         )'''
-        input_insert = "insert into users(name, password) values(?, ?)"
+        input_insert = "insert into users(name, password, email) values(?, ?, ?)"
         name = request.form['name']
-        cursor.execute(table)
-        cursor.execute(input_insert, (name, "random"))
-        connection.commit()
-        connection.close()
-        return f'Hello, {name}'
+        password = request.form['password']
+        email = request.form['email']
+        table_username = request.form.get("name")
+        table_password = (request.form.get("password",) or "").strip()
+        table_email = request.form.get("email")
+        if not table_username:
+            error_name = "Username is required"
+            return render_template("register.html", error_name=error_name)
+        elif not table_password:
+            error_pass = "Password is required"
+            return render_template("register.html", table_username=table_username, error_pass=error_pass)
+        elif not table_email:
+            error_email = "Email is required"
+            return render_template("register.html", table_username=table_username, table_password=table_password, error_email=error_email)
+        else:
+            cursor.execute(table)
+            cursor.execute(input_insert, (name, password, email))
+            connection.commit()
+            connection.close()
+            return f'Hello, {name}'
+
     return render_template('register.html')
     
 
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -2,8 +2,10 @@ var button= document.getElementById('button')
 
 function Vendor() {
     button.style.left = '115px'
+    document.getElementById("vendor_inputs").style.display = "block";
 }
 
 function Student() {
     button.style.left = '0px'
+    document.getElementById("vendor_inputs").style.display = "none";
 }
